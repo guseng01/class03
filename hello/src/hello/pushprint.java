73582e332e6ee1eae4915386382c4ab76f9b0e8a
So Jeong <thwjd5580@naver.com>
2019-11-15 13:37:40 +0900

    asd

--- hello/src/hello/pushprint.java
+++ hello/src/hello/pushprint.java
@@ -3,7 +3,7 @@ package hello;
 public class pushprint {
 
 	public static void main(String[] args) {
-		System.out.println("add java class file puch");
+		System.out.println("add java class file puch.");
 	}
 
 }
